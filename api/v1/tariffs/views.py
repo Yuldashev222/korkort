b3e46fd882a8bde524d1b1cedfd55256e23f6a4c
yuldashev222 <oybekyuldashov54@gmail.com>
2023-08-10 16:20:08 +0500

    asd

--- api/v1/tariffs/views.py
+++ api/v1/tariffs/views.py
@@ -1,12 +1,14 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from .models import Tariff, TariffInfo
 from .serializers import TariffSerializer, TariffInfoSerializer
+from api.v1.accounts.permissions import IsStudent
 
 
 class TariffAPIView(GenericViewSet):
-    permission_classes = ()
+    permission_classes = [IsAuthenticated, IsStudent]
     serializer_class = TariffSerializer
     queryset = Tariff.objects.filter(is_active=True).order_by('-created_at')
 
