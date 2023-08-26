03f6de6b3e0e669657ff30506e19419b9505ffdb
yuldashev222 <oybekyuldashov54@gmail.com>
2023-08-26 15:33:13 +0500

    asd

--- api/v1/accounts/signals.py
+++ api/v1/accounts/signals.py
@@ -1,7 +1,9 @@
-from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils.timezone import now
+from django.db.models.signals import post_save
 
 from api.v1.accounts.tasks import add_student_lessons
+from api.v1.lessons.models import LessonStudentStatisticsByDay
 from api.v1.accounts.models import CustomUser
 
 
@@ -9,3 +11,6 @@ from api.v1.accounts.models import CustomUser
 def add_lessons(instance, created, *args, **kwargs):
     if created and not instance.is_staff:
         add_student_lessons.delay(instance.id)
+        today_week_day = now().date().weekday()
+        objs = [LessonStudentStatisticsByDay(student=instance, date=today_week_day - i) for i in [0, 1, 2, 3, 4, 5, 6]]
+        LessonStudentStatisticsByDay.objects.bulk_create(objs)
