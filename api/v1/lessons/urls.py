03fb7ec1b755f4418e5e656f799d0994961d5b07
yuldashev222 <oybekyuldashov54@gmail.com>
2023-09-12 10:18:59 +0500

    asd

--- api/v1/lessons/urls.py
+++ api/v1/lessons/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from api.v1.lessons.views import LessonAnswerAPIView, LessonAPIView, LessonStudentStatisticsByDayAPIView
 
 urlpatterns = [
-    # path('<int:pk>/', LessonAPIView.as_view({'get': 'retrieve'})),
+    path('<int:pk>/', LessonAPIView.as_view({'get': 'retrieve'})),
     path('statistics/', LessonStudentStatisticsByDayAPIView.as_view({'get': 'list'})),
     path('questions/answers/', LessonAnswerAPIView.as_view())
 ]
