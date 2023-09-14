163b3b3c86789307b013e3fddd3249aac773114c
yuldashev222 <oybekyuldashov54@gmail.com>
2023-09-14 14:38:27 +0500

    asd

--- api/v1/questions/serializers/questions.py
+++ api/v1/questions/serializers/questions.py
@@ -11,9 +11,9 @@ class QuestionSerializer(serializers.Serializer):
     category = serializers.StringRelatedField()
     question_text = serializers.SerializerMethodField()
     # question_video = serializers.FileField(source='video')
-    question_video = serializers.URLField(default='https://e-report.uz/media/questions/18000%3A%200fac9957-9516-4083-9ae4/videos/giphy.gif')
+    question_video = serializers.URLField(default='https://www.e-report.uz/media/questions/18000%3A%200fac9957-9516-4083-9ae4/videos/giphy.gif')
     # question_image = serializers.ImageField(source='image')
-    question_image = serializers.URLField(default='https://e-report.uz/media/chapters/1:%20836c4b38-fe8e-4ef2-9a9c-bab/lessons/1:%20905c9192-956f-4054-9ce1-161/images/Re_A8H4vJl.png')
+    question_image = serializers.URLField(default='https://www.e-report.uz/media/chapters/1:%20836c4b38-fe8e-4ef2-9a9c-bab/lessons/1:%20905c9192-956f-4054-9ce1-161/images/Re_A8H4vJl.png')
     is_saved = serializers.SerializerMethodField()  # last
 
     variant_set = VariantSerializer(many=True)
