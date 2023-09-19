7fd8b9c4c554c56ae03099fbef048c2e2bfd8b3a
yuldashev222 <oybekyuldashov54@gmail.com>
2023-09-19 16:02:16 +0500

    asd

--- api/v1/questions/serializers/questions.py
+++ api/v1/questions/serializers/questions.py
@@ -13,7 +13,7 @@ class QuestionSerializer(serializers.Serializer):
     # question_gif = serializers.FileField(source='gif')
     question_gif = serializers.URLField(default='http://51.20.2.33/media/questions/6000%3A_73f57c8b-13ed-447f-b39c-/gifs/giphy.gif')
     # gif_last_frame_number = serializers.IntegerField()
-    gif_last_frame_number = serializers.IntegerField(default=24)
+    question_gif_last_frame_number = serializers.IntegerField(default=24)
     # question_image = serializers.ImageField(source='image')
     question_image = serializers.URLField(default='http://51.20.2.33/media/chapters/1:_df478f64-8c95-4fe0-a9d2-30e/lessons/1:%208908c739-1de4-4f39-9d38-929/images/Re_z4gl9tD.png')
     is_saved = serializers.SerializerMethodField()
