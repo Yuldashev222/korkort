e7a6f42d793688cd9bbca7581b8bdbf00b38327d
yuldashev222 <oybekyuldashov54@gmail.com>
2023-09-23 10:43:17 +0500

    asd

--- api/v1/swish/views.py
+++ api/v1/swish/views.py
@@ -1,10 +1,11 @@
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 
+from api.v1.swish.permissions import GtMinBonusMoney
 from api.v1.swish.serializers import SwishCardSerializer
 from api.v1.accounts.permissions import IsStudent
 
 
 class SwishCardAPIView(CreateAPIView):
-    permission_classes = (IsAuthenticated, IsStudent)
+    permission_classes = (IsAuthenticated, IsStudent, GtMinBonusMoney)
     serializer_class = SwishCardSerializer
