c0a713ccb9e6ed2b13335053102f9bdb393980c5
yuldashev222 <oybekyuldashov54@gmail.com>
2023-09-25 15:03:30 +0500

    asd

--- api/v1/tariffs/urls.py
+++ api/v1/tariffs/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from .views import TariffAPIView
 
 urlpatterns = [
-    path('', TariffAPIView.as_view({'get': 'list'}), name='tariff-list'),
-    path('<int:pk>/', TariffAPIView.as_view({'get': 'retrieve'}), name='tariff-detail')
+    path('', TariffAPIView.as_view(), name='tariff-list'),
 ]
--- api/v1/tariffs/views.py
+++ api/v1/tariffs/views.py
@@ -1,8 +1,6 @@
-from django.core.cache import cache
-from django.utils.timezone import now
+from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet
 
 from .models import Tariff
 from .serializers import TariffSerializer
@@ -10,34 +8,35 @@ from api.v1.accounts.permissions import IsStudent
 from api.v1.discounts.models import StudentDiscount, TariffDiscount
 
 
-class TariffAPIView(GenericViewSet):
+class TariffAPIView(GenericAPIView):
     permission_classes = [IsAuthenticated, IsStudent]
     serializer_class = TariffSerializer
     queryset = Tariff.objects.filter(is_active=True)
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
         student = self.request.user
 
-        student_discount = StudentDiscount.get_student_discount()
-        student_discount_value = 0
-        student_discount_is_percent = False
-        if student_discount:
-            student_discount_value = student_discount.get('discount_value')
-            student_discount_is_percent = student_discount.get('is_percent')
-
-        tariff_discount = TariffDiscount.get_tariff_discount()
         tariff_discount_value = 0
         tariff_discount_is_percent = False
         tariff_discount_title = ''
         tariff_discount_image = None
-        # if tariff_discount and tariff_discount['valid_to'] > now().date():
+        student_discount_value = 0
+        student_discount_is_percent = False
+
+        student_discount = StudentDiscount.get_student_discount()
+        tariff_discount = TariffDiscount.get_tariff_discount()
+
+        if student_discount:
+            student_discount_value = student_discount['discount_value']
+            student_discount_is_percent = student_discount['is_percent']
+
         if tariff_discount:
-            tariff_discount_value = tariff_discount.get('discount_value')
-            tariff_discount_is_percent = tariff_discount.get('is_percent')
-            tariff_discount_title = tariff_discount.get('title')
-            tariff_discount_image = tariff_discount.get('image_url')
+            tariff_discount_value = tariff_discount['discount_value']
+            tariff_discount_is_percent = tariff_discount['is_percent']
+            tariff_discount_title = tariff_discount['title']
+            tariff_discount_image = tariff_discount['image_url']
 
         return Response({
             'student_discount_value': student_discount_value,
@@ -45,7 +44,8 @@ class TariffAPIView(GenericViewSet):
             'tariff_discount_value': tariff_discount_value,
             'tariff_discount_is_percent': tariff_discount_is_percent,
             'tariff_discount_title': tariff_discount_title,
-            'tariff_discount_image': request.build_absolute_uri(tariff_discount_image),
+            # 'tariff_discount_image': request.build_absolute_uri(tariff_discount_image),
+            'tariff_discount_image': 'https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg',
             'student_bonus_money': student.bonus_money,
             'objects': serializer.data
         })
