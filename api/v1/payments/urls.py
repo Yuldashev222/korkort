99c072335e84af926b5af138d44f44e28f61f0e8
yuldashev222 <oybekyuldashov54@gmail.com>
2023-10-04 18:42:56 +0500

    asd

--- api/v1/payments/urls.py
+++ api/v1/payments/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 
 from .views import OrderAPIView, CheckCouponAPIView
 from .checkout import StripeCheckoutAPIView
-from .webhooks import StripeWebhookView
+from .webhooks import stripe_webhook_view  # , StripeWebhookView
 
 urlpatterns = [
     path('stripe/create-checkout-session/', StripeCheckoutAPIView.as_view()),
-    path('stripe/webhooks/', StripeWebhookView.as_view(), name='stripe-webhook'),
+    # path('stripe/webhooks/', StripeWebhookView.as_view(), name='stripe-webhook'),
+    path('stripe/webhooks/', stripe_webhook_view, name='stripe-webhook'),
     path('orders/', OrderAPIView.as_view({'get': 'list'}), name='order-list'),
     path('check-coupon/', CheckCouponAPIView.as_view()),
     path('orders/<int:pk>/', OrderAPIView.as_view({'get': 'retrieve'}), name='order-detail'),
--- api/v1/payments/webhooks.py
+++ api/v1/payments/webhooks.py
@@ -1,47 +1,43 @@
 import stripe
-from django.conf import settings
-from django.http import HttpResponse
-from django.views import View
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
-
-from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
-from .models import Order
+from api.v1.payments.models import Order
+from config import settings
+
+import json
+from django.http import HttpResponse
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
+endpoint_secret = 'whsec_e9092e9da384c906358bb1684aee480f059eba910ba30f13c11d4f97604994b0'
 
 
-@method_decorator(csrf_exempt, name="dispatch")
-class StripeWebhookView(View):
-    def post(self, request, *args, **kwargs):
-        payload = request.body
-        endpoint_secret = settings.STRIPE_WEBHOOK_SECRET
-        sig_header = request.META["HTTP_STRIPE_SIGNATURE"]
-        try:
-            event = stripe.Webhook.construct_event(payload, sig_header, endpoint_secret)
-        except ValueError as e:
-            # Invalid payload
-            return HttpResponse(content=str(e), status=400)
-        except stripe.error.SignatureVerificationError as e:
-            # Invalid signature
-            return HttpResponse(content=str(e), status=400)
+# Using Django
+@csrf_exempt
+def stripe_webhook_view(request):
+    payload = request.body
 
-        if event.type == "checkout.session.completed":
-            session = event.data.object
-            if session.mode == 'payment' and session.payment_status == 'paid':
-                try:
-                    order = Order.objects.select_related('student').get(id=session.client_reference_id, is_paid=False)
-                except Order.DoesNotExist as e:
-                    return HttpResponse(content=str(e), status=400)
-                order.is_paid = True
-                order.stripe_id = session.payment_intent
-                order.save()
+    try:
+        event = stripe.Event.construct_from(json.loads(payload), stripe.api_key)
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
 
-                student = order.student
-                message = render_to_string('payments/checkout.html')
-                send_mail('Hello Everyone', message, settings.DEFAULT_FROM_EMAIL, recipient_list=[student.email],
-                          html_message=message)
+    if event.type == "checkout.session.completed":
+        session = event.data.object
+        if session.mode == 'payment' and session.payment_status == 'paid':
+            try:
+                order = Order.objects.select_related('student').get(id=session.client_reference_id, is_paid=False)
+            except Order.DoesNotExist as e:
+                return HttpResponse(content=str(e), status=400)
+            order.is_paid = True
+            order.stripe_id = session.payment_intent
+            order.save()
 
-        return HttpResponse(status=200)
+            student = order.student
+            message = render_to_string('payments/checkout.html')
+            send_mail('Hello Everyone', message, settings.DEFAULT_FROM_EMAIL, recipient_list=[student.email],
+                      html_message=message)
+
+    return HttpResponse(status=200)
