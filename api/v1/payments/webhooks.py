2bb39d498881e2cfc3a3003054180bac6ca06d7b
yuldashev222 <oybekyuldashov54@gmail.com>
2023-10-07 17:28:53 +0500

    asd

--- api/v1/payments/webhooks.py
+++ api/v1/payments/webhooks.py
@@ -2,41 +2,42 @@ import json
 import stripe
 from config import settings
 from django.http import HttpResponse
-from django.core.mail import send_mail
-from django.template.loader import render_to_string
 from django.views.decorators.csrf import csrf_exempt
-
-from api.v1.payments.models import Order
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 endpoint_secret = settings.STRIPE_WEBHOOK_SECRET
 
 
-# Using Django
 @csrf_exempt
 def stripe_webhook_view(request):
-    payload = request.body
+    event = None
+    payload = request.data
+    sig_header = request.headers['STRIPE_SIGNATURE']
 
     try:
-        event = stripe.Event.construct_from(json.loads(payload), stripe.api_key)
+        event = stripe.Webhook.construct_event(payload, sig_header, endpoint_secret)
     except ValueError as e:
-        # Invalid payload
-        return HttpResponse(status=400)
+        raise HttpResponse(str(e), status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        raise HttpResponse(str(e), status=400)
 
-    if event.type == "checkout.session.completed":
-        session = event.data.object
-        if session.mode == 'payment' and session.payment_status == 'paid':
-            try:
-                order = Order.objects.select_related('student').get(id=session.client_reference_id, is_paid=False)
-            except Order.DoesNotExist as e:
-                return HttpResponse(content=str(e), status=404)
-            order.is_paid = True
-            order.stripe_id = session.payment_intent
-            order.save()
-
-            student = order.student
-            message = render_to_string('payments/checkout.html')
-            send_mail(subject='Hello Everyone', message=message, from_email=None, recipient_list=[student.email],
-                      html_message=message)
+    # Handle the event
+    if event['type'] == 'checkout.session.completed':
+        print('================================================')
+        # session = event['data']['object']
+        # if session.mode == 'payment' and session.payment_status == 'paid':
+        #     try:
+        #         order = Order.objects.select_related('student').get(id=session.client_reference_id, is_paid=False)
+        #     except Order.DoesNotExist as e:
+        #         return HttpResponse(content=str(e), status=404)
+        #     order.is_paid = True
+        #     order.stripe_id = session.payment_intent
+        #     order.save()
+        #
+        #     student = order.student
+        #     message = render_to_string('payments/checkout.html')
+        #     send_mail(subject='Hello Everyone', message=message, from_email=None, recipient_list=[student.email],
+        #               html_message=message)
 
     return HttpResponse(status=200)
--- config/settings.py
+++ config/settings.py
@@ -128,7 +128,6 @@ MEDIA_URL = 'media/'
 LOGIN_REDIRECT_URL = '/api/v1/accounts/profile'
 
 STATIC_ROOT = BASE_DIR / 'static'
-#STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
@@ -191,8 +190,8 @@ CELERY_TASK_TIME_LIMIT = 30 * 60  # last
 CORS_ALLOW_ALL_ORIGINS = True
 
 # stripe
-STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY_TEST')
-STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY_TEST')
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY_LIVE')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY_LIVE')
 STRIPE_WEBHOOK_SECRET = os.environ.get('STRIPE_WEBHOOK_SECRET')
 STRIPE_CHECKOUT_TIMEOUT = 30 * 60  # in second
 STRIPE_CHECKOUT_CURRENCY = 'SEK'
@@ -278,68 +277,12 @@ JAZZMIN_SETTINGS = {
     "copyright": 'test copyright',
 }
 
-# CKEDITOR_CONFIGS = {
-#     'default': {
-#         # 'skin': 'moono',
-#         # 'skin': 'office2013',
-#         'toolbar_Basic': [
-#             ['Source', '-', 'Bold', 'Italic']
-#         ],
-#         'toolbar_YourCustomToolbarConfig': [
-#             {'name': 'document', 'items': ['Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates']},
-#             {'name': 'clipboard', 'items': ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo']},
-#             {'name': 'editing', 'items': ['Find', 'Replace', '-', 'SelectAll']},
-#             {'name': 'forms',
-#              'items': ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton',
-#                        'HiddenField']},
-#             '/',
-#             {'name': 'basicstyles',
-#              'items': ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat']},
-#             {'name': 'paragraph',
-#              'items': ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-',
-#                        'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl',
-#                        'Language']},
-#             {'name': 'links', 'items': ['Link', 'Unlink', 'Anchor']},
-#             {'name': 'insert',
-#              'items': ['Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe']},
-#             '/',
-#             {'name': 'styles', 'items': ['Styles', 'Format', 'Font', 'FontSize']},
-#             {'name': 'colors', 'items': ['TextColor', 'BGColor']},
-#             {'name': 'tools', 'items': ['Maximize', 'ShowBlocks']},
-#             {'name': 'about', 'items': ['About']},
-#             '/',  # put this to force next toolbar on new line
-#             {'name': 'yourcustomtools', 'items': [
-#                 # put the name of your editor.ui.addButton here
-#                 'Preview',
-#                 'Maximize',
-#                 'Youtube'
-#             ]},
-#         ],
-#         'toolbar': 'YourCustomToolbarConfig',  # put selected toolbar config here
-#         # 'toolbarGroups': [{ 'name': 'document', 'groups': [ 'mode', 'document', 'doctools' ] }],
-#         # 'height': 291,
-#         # 'width': '100%',
-#         # 'filebrowserWindowHeight': 725,
-#         # 'filebrowserWindowWidth': 940,
-#         # 'toolbarCanCollapse': True,
-#         # 'mathJaxLib': '//cdn.mathjax.org/mathjax/2.2-latest/MathJax.js?config=TeX-AMS_HTML',
-#         'tabSpaces': 4,
-#         'extraPlugins': ','.join([
-#             'uploadimage',  # the upload image feature
-#             # your extra plugins here
-#             'div',
-#             'autolink',
-#             'autoembed',
-#             'embedsemantic',
-#             'autogrow',
-#             # 'devtools',
-#             'widget',
-#             'lineutils',
-#             'clipboard',
-#             'dialog',
-#             'dialogui',
-#             'elementspath',
-#             'youtube'
-#         ]),
+# LOGGING = {
+#     'loggers': {
+#         'gunicorn.access': {
+#             'level': 'DEBUG',
+#             'handlers': ['console'],
+#             'propagate': False
+#         }
 #     }
 # }
