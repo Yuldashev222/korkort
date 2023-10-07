4a005680ec6130713d246cb8410da1403fcad355
yuldashev222 <oybekyuldashov54@gmail.com>
2023-10-07 19:02:30 +0500

    asd

--- api/v1/payments/webhooks.py
+++ api/v1/payments/webhooks.py
@@ -1,4 +1,7 @@
 import stripe
+from django.utils.decorators import method_decorator
+from django.views import View
+
 from config import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -7,36 +10,38 @@ stripe.api_key = settings.STRIPE_SECRET_KEY
 endpoint_secret = settings.STRIPE_WEBHOOK_SECRET
 
 
-@csrf_exempt
-def stripe_webhook_view(request):
-    event = None
-    payload = request.body
-    sig_header = request.META["HTTP_STRIPE_SIGNATURE"]
+@method_decorator(csrf_exempt, name="dispatch")
+class StripeWebhookView(View):
+    def post(self, request, *args, **kwargs):
+        event = None
+        payload = request.body
+        sig_header = request.META['HTTP_STRIPE_SIGNATURE']
 
-    try:
-        event = stripe.Webhook.construct_event(payload, sig_header, endpoint_secret)
-    except ValueError as e:
-        raise HttpResponse(str(e), status=400)
-    except stripe.error.SignatureVerificationError as e:
-        # Invalid signature
-        raise HttpResponse(str(e), status=400)
+        try:
+            event = stripe.Webhook.construct_event(payload, sig_header, endpoint_secret)
+        except ValueError as e:
+            return HttpResponse(str(e), status=400)
 
-    # Handle the event
-    if event['type'] == 'checkout.session.completed':
-        print('================================================')
-        # session = event['data']['object']
-        # if session.mode == 'payment' and session.payment_status == 'paid':
-        #     try:
-        #         order = Order.objects.select_related('student').get(id=session.client_reference_id, is_paid=False)
-        #     except Order.DoesNotExist as e:
-        #         return HttpResponse(content=str(e), status=404)
-        #     order.is_paid = True
-        #     order.stripe_id = session.payment_intent
-        #     order.save()
-        #
-        #     student = order.student
-        #     message = render_to_string('payments/checkout.html')
-        #     send_mail(subject='Hello Everyone', message=message, from_email=None, recipient_list=[student.email],
-        #               html_message=message)
+        except stripe.error.SignatureVerificationError as e:
+            # Invalid signature
+            return HttpResponse(str(e), status=400)
 
-    return HttpResponse(status=200)
+        # Handle the event
+        if event['type'] == 'checkout.session.completed':
+            print('================================================')
+            # session = event['data']['object']
+            # if session.mode == 'payment' and session.payment_status == 'paid':
+            #     try:
+            #         order = Order.objects.select_related('student').get(id=session.client_reference_id, is_paid=False)
+            #     except Order.DoesNotExist as e:
+            #         return HttpResponse(content=str(e), status=404)
+            #     order.is_paid = True
+            #     order.stripe_id = session.payment_intent
+            #     order.save()
+            #
+            #     student = order.student
+            #     message = render_to_string('payments/checkout.html')
+            #     send_mail(subject='Hello Everyone', message=message, from_email=None, recipient_list=[student.email],
+            #               html_message=message)
+
+        return HttpResponse(status=200)
