bf28c72cf424c45324e9c13051a655645de324c2
yuldashev222 <oybekyuldashov54@gmail.com>
2023-11-01 16:10:07 +0500

    language ordering number order_by

--- api/v1/languages/admin.py
+++ api/v1/languages/admin.py
@@ -7,3 +7,4 @@ from api.v1.languages.models import Language
 class LanguageAdmin(admin.ModelAdmin):
     list_display = ['language_id', 'name', 'ordering_number', 'is_active']
     list_display_links = ['language_id', 'name']
+    ordering = ['ordering_number']
